2a74d4ccd664d3ab194e175b30589f3e9e8c7ec5
Fernando <fernandocajaschuqui@hotmail.com>
2021-07-13 18:38:15 -0500

    Primer commit del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { MuertesCovidComponent } from './muertes-covid/muertes-covid.component';
+import { FoundpagesComponent } from './foundpages/foundpages.component';
+import { PresentacionComponent } from './presentacion/presentacion.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    MuertesCovidComponent,
+    FoundpagesComponent,
+    PresentacionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
